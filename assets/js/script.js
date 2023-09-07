176869bdbc4a3fd4c3550023b95d52f11c2cd616
Hamed <hamedpyn@gmail.com>
2023-09-08 02:43:35 +0430

    adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// * Developer : @HAMEDPYN
+// * Version : 1.0
+// * Date : 08 / 10 / 2023
+
+// * Description : This is a simple program that will help you to get the the words meaning and its definitions, It also tells how to pronounce it and you can Listen it
+
 // Select DOM Elements
 
 const searchBtn = document.querySelector("#search-btn");
@@ -12,47 +18,65 @@ const resultSec = document.querySelector("#result");
 const wait = document.querySelector("#wait");
 
 
+// Function to handle the dictionary lookup
 const dictionary = () => {
+    // Get the input value
     let inValue = inpWord.value;
+    // if it's not empty 
     if (inValue) {
-        resultSec.style.display = "none"
+        resultSec.style.display = "none";
 
         fetchData(inValue);
-    
+
+        // empty the input value
         inpWord.value = "";
-    }else {
-        wait.style.display = "block"
-        wait.innerHTML = "Please Enter a Word..."
+    } else {
+        // if it's empty , show an alert text
+        wait.style.display = "block";
+        wait.innerHTML = "Please Enter a Word...";
     }
 };
 
+// Function to fetch data from the dictionary API
 function fetchData(inValue) {
-    wait.style.display = "block"
-    wait.innerHTML = "Waiting..."
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inValue}`).then(res => res.json())
+    wait.style.display = "block";
+    wait.innerHTML = "Waiting...";
+
+    // Make a GET request to the dictionary API
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inValue}`)
+        .then(res => res.json()) // Parse the response as JSON
         .then(result => {
-            let { word, phonetic, meanings } = result[0]
-            let { definitions } = result[0].meanings[0]
+            // Extract relevant data from the response
+            let { word, phonetic, meanings } = result[0];
+            let { definitions } = result[0].meanings[0];
 
-            enWord.innerHTML = word
-            pronouns.innerHTML = phonetic
-            details.innerHTML = meanings[0].partOfSpeech
-            wordMeaning.innerHTML = definitions[0].definition
+            // Display the word, pronunciation, part of speech, and definition
+            enWord.innerHTML = word;
+            pronouns.innerHTML = phonetic;
+            details.innerHTML = meanings[0].partOfSpeech;
+            wordMeaning.innerHTML = definitions[0].definition;
 
+            // Display the example if available, or a message if not
             if (definitions[0].example == undefined) {
-                wordExample.innerHTML = "There is no Example for this word in our Dictionary."
+                wordExample.innerHTML = "There is no Example for this word in our Dictionary.";
             } else {
-                wordExample.innerHTML = definitions[0].example
+                wordExample.innerHTML = definitions[0].example;
             }
 
+            // Show the result section and hide the loading message
             resultSec.style.display = "block";
-            wait.style.display = "none"
-        })
+            wait.style.display = "none";
+        });
 }
 
+// Add a click event listener to the "voice button ( SPEAKER )"
 voice.addEventListener('click', () => {
-    let speechVoice = new SpeechSynthesisUtterance(enWord.innerHTML)
-    speechSynthesis.speak(speechVoice)
+    // Create a new SpeechSynthesisUtterance object with the content of enWord.innerHTML
+    let speechVoice = new SpeechSynthesisUtterance(enWord.innerHTML);
+
+    // Use the speechSynthesis API to speak the speechVoice
+    speechSynthesis.speak(speechVoice);
 });
 
+// Add a click event listener to the search button
 searchBtn.addEventListener('click', dictionary);
