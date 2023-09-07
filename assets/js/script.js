10e64abeaa8e2b99353d62d2ee4a88413ac7f49e
Hamed <hamedpyn@gmail.com>
2023-09-08 01:27:13 +0430

    Select DOM Elements

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Select DOM Elements
+
+const searchBtn = document.querySelector("#search-btn");
+const inpWord = document.querySelector("#inp-word");
+const voice = document.querySelector(".word button");
+const word = document.querySelector(".word h3");
+const details = document.querySelector(".details").firstChild;
+const pronunciation = document.querySelector(".details").lastChild;
+const wordMeaning = document.querySelector('.word-meaning');
+const wordExample = document.querySelector(".word-example");
